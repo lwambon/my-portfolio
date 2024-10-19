bfe547f45933454b9650e56c1f6591289163e634
lwambon <lwambonyaga@gmail.com>
2024-10-19 22:20:26 +0300

    hero section of my portofolio website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./component/Header/Header";
+import Hero from "./component/Hero/Hero";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Hero />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import "./Hero.css";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import HeroImage from "./../../assets/lucy-wambo.jpg";
+
+const GithubUrl = "https://github.com/lwambon";
+const LinkedinUrl = "https://www.linkedin.com/in/lucy-wambui-225965242";
+
+function Hero() {
+  return (
+    <div className="hero-section">
+      <div className="hero-container">
+        <p className="header-hero">Hello..</p>
+        <p className="hero-content">
+          I Am <span>lucy wambui</span>{" "}
+        </p>
+        <p className="hero-content">
+          a <span>software engineer,</span>
+        </p>
+        <p className="hero-content">welcome to my website</p>
+        <div className="hero-links">
+          <button>download resume</button>
+          <div className="link-items">
+            <Link to={GithubUrl} target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </Link>
+          </div>
+          <div className="link-items">
+            <Link to={LinkedinUrl} target="_blank" rel="noopener noreferrer">
+              <FaLinkedinIn />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src={HeroImage} alt="Lucy Wambui" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
